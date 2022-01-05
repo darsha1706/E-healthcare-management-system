42173ea01e93f7353e04d5d574a042ed5da5dd6a
Darshana Agarwal <darshana170699@gmail.com>
2022-01-05 17:50:20 +0530

    java project created

---
+++ EHospitalManagementApplication.java
@@ -0,0 +1,29 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.project.demo.dao.User;
+import com.project.demo.repository.AdminRepo;
+
+
+@SpringBootApplication
+public class EHospitalManagementApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context=	SpringApplication.run(EHospitalManagementApplication.class, args);
+
+		
+		AdminRepo crepo=context.getBean(AdminRepo.class);
+		
+		try {
+			crepo.deleteAll();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		crepo.save(new User("kghogare5@gmail.com", "Kshitij@123", "Kshitij", "Ghogare",
+				"7030750693", "Admin"));
+	}
+
+}
